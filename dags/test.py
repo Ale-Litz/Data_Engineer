2db65b5b086390c31e6c679738b5a9220312717f
Alexandre LITZ <77252253+Ale-Litz@users.noreply.github.com>
2023-09-18 17:12:34 -0300

    Update test.py

--- dags/test.py
+++ dags/test.py
@@ -1,1 +1,24 @@
-import pandas as pd
+from hello_operator import HelloOperator
+from datetime import datetime, timedelta
+from airflow import DAG
+
+with DAG(
+  dag_id="test_operator_hello",
+
+  default_args={
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5)
+  }
+
+  description="test consult dag",
+  schedule=None,
+  start)date=datetime(2023, 1, 1),
+  catchup=False,
+  tags=["DAG test"]
+  
+) as dag:
+
+  hello_task = HelloOperator(task_id="sample-task", name="data_procurando_entios")
+
+  hello_task
